9120b7964b65529dccd1843159f0a700f51b3631
bodnarb.04 <bodnarb.04@A321-13>
2022-03-21 12:45:01 +0100

    Hotova03

---
+++ Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DatumCas03
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        DateTime startTime;
+        DateTime endtime;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            button1.Enabled = false;
+            button2.Enabled = true;
+            startTime = DateTime.Now;
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            button1.Enabled = true;
+            button2.Enabled = false;
+
+            endtime = DateTime.Now;
+
+            TimeSpan duration = endtime - startTime;
+
+            MessageBox.Show("Uplynula doba je " + duration.Seconds);
+        }
+    }
+}
